6d37f2bba2c311e332db5da58f12ae8f6150b74a
juanBrusatti <mjuanmolina123@gmail.com>
2026-02-17 19:44:23 -0300

    improvement el SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -91,13 +91,6 @@ export const metadata: Metadata = {
   robots: {
     index: true,
     follow: true,
-    googleBot: {
-      index: true,
-      follow: true,
-      'max-video-preview': -1,
-      'max-image-preview': 'large',
-      'max-snippet': -1,
-    },
   },
   verification: {
     google: 'google-site-verification-code',
