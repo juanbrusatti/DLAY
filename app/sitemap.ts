0c7cd5dbffd58a94102992e0265c5ad5c320fef3
juanBrusatti <mjuanmolina123@gmail.com>
2026-02-17 20:07:15 -0300

    improvement el SEO

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -11,6 +11,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 1,
     },
     {
+      url: `${baseUrl}/servicios`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.9,
+    },
+    {
       url: `${baseUrl}/about-dlay`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
